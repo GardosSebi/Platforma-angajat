3f5aa58dbbb980c2708e2dd8551fcddf56470012
alexghb <alex@ghb-software.com>
2026-05-12 18:12:48 +0300

    fix chatbot

--- apps/api/prisma/migrations/20260512093454_chatbot/migration.sql
+++ apps/api/prisma/migrations/20260512093454_chatbot/migration.sql
@@ -1,8 +1,26 @@
 -- RenameIndex
-ALTER INDEX "CommunicationAnnouncement_tenantId_audienceType_audienceRefId_i" RENAME TO "CommunicationAnnouncement_tenantId_audienceType_audienceRef_idx";
+DO $$
+BEGIN
+  IF to_regclass('"CommunicationAnnouncement_tenantId_audienceType_audienceRefId_i"') IS NOT NULL
+    AND to_regclass('"CommunicationAnnouncement_tenantId_audienceType_audienceRef_idx"') IS NULL THEN
+    ALTER INDEX "CommunicationAnnouncement_tenantId_audienceType_audienceRefId_i" RENAME TO "CommunicationAnnouncement_tenantId_audienceType_audienceRef_idx";
+  END IF;
+END $$;
 
 -- RenameIndex
-ALTER INDEX "CommunicationAnnouncementRead_tenantId_announcementId_readAt_id" RENAME TO "CommunicationAnnouncementRead_tenantId_announcementId_readA_idx";
+DO $$
+BEGIN
+  IF to_regclass('"CommunicationAnnouncementRead_tenantId_announcementId_readAt_id"') IS NOT NULL
+    AND to_regclass('"CommunicationAnnouncementRead_tenantId_announcementId_readA_idx"') IS NULL THEN
+    ALTER INDEX "CommunicationAnnouncementRead_tenantId_announcementId_readAt_id" RENAME TO "CommunicationAnnouncementRead_tenantId_announcementId_readA_idx";
+  END IF;
+END $$;
 
 -- RenameIndex
-ALTER INDEX "SsmPsiEquipmentVerification_tenantId_equipmentId_performedAt_id" RENAME TO "SsmPsiEquipmentVerification_tenantId_equipmentId_performedA_idx";
+DO $$
+BEGIN
+  IF to_regclass('"SsmPsiEquipmentVerification_tenantId_equipmentId_performedAt_id"') IS NOT NULL
+    AND to_regclass('"SsmPsiEquipmentVerification_tenantId_equipmentId_performedA_idx"') IS NULL THEN
+    ALTER INDEX "SsmPsiEquipmentVerification_tenantId_equipmentId_performedAt_id" RENAME TO "SsmPsiEquipmentVerification_tenantId_equipmentId_performedA_idx";
+  END IF;
+END $$;
